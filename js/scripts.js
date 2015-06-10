7abec0efbd106967a7f39c50cea909c8d370454b
Sathish DK <sathishdk@sathishdk.com>
2015-06-10 14:31:12 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function hello(){
- var username = document.getElementByID('username');
- var password = document.getElementByID('password');
+ var username = document.getElementByID('username').val;
+ var password = document.getElementByID('password').val;
  var message = "USERNAME: " + username + " & PASSWORD: " + password;
  console.log(message);
 }
